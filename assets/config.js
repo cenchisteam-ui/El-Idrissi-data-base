e6891918b107543a5e96b1e543a0098aa13804f1
cenchisteam-ui <cenchisteam@gmail.com>
2026-07-03 23:29:47 +0100

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,7 +1,7 @@
 // ============================================================
 // FILL THIS IN — see README.md for step-by-step instructions
 // ============================================================
-window.SUPABASE_URL = "https://omlkaespfwjmdadtvwdu.supabase.co/rest/v1/";
+window.SUPABASE_URL = "https://omlkaespfwjmdadtvwdu.supabase.co";
 window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9tbGthZXNwZndqbWRhZHR2d2R1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMwMjI1MTEsImV4cCI6MjA5ODU5ODUxMX0.p96dfhxAQs_H7tdVYSrLlHnfXDB_Zq55hqtpTfF1bPw";
 
 // Name of the storage bucket you create for uploaded files
